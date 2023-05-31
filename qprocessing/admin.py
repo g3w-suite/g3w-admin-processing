cbb4dc7acfa9fa5a2d1626614f1f0a1cf1559943
wlorenzetti <lorenzett@gis3w.it>
2023-05-31 15:58:59 +0200

    Add a base admin for QprocessingProject model.

--- qprocessing/admin.py
+++ qprocessing/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import QProcessingProject
 
-# Register your models here.
+@admin.register(QProcessingProject)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'model',
+    )
