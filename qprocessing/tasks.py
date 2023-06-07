39c08e060c0762dcbf74b625448eef11b1e522c6
wlorenzetti <lorenzett@gis3w.it>
2023-06-07 11:23:14 +0200

    Refine consumer tasks module.

--- qprocessing/tasks.py
+++ qprocessing/tasks.py
@@ -10,35 +10,89 @@ __date__ = '2023-05-23'
 __copyright__ = 'Copyright 2015 - 2023, Gis3w'
 __license__ = 'MPL 2.0'
 
+from functools import wraps
 
-from huey.contrib.djhuey import HUEY, db_task
+from django.db import close_old_connections
+from huey.contrib.djhuey import HUEY
 from huey_monitor.tqdm import ProcessInfo
+from celery import shared_task, current_task
+from celery.utils.log import get_task_logger
 from .models import QProcessingProject
+from .utils.data import QProcessingModel
 from qgis.core import QgsProcessingContext, QgsProcessingFeedback
 
 
 task = HUEY.task
+
+celery_logger = get_task_logger(__name__)
+
+def run_model_test(qprocessing_project_pk, project_pk, params):
+
+
+    print(qprocessing_project_pk)
+    qpp = QProcessingProject.objects.get(pk=qprocessing_project_pk)
+    prj = qpp.projects.get(pk=project_pk)
+    print(prj)
+    print(prj.qgis_project)
+
+
+    return True
 
 def run_model(qprocessing_project_pk, project_pk, params):
     """
     Run processing model
     """
 
-    qpm = QProcessingProject.objects.get(pk=qprocessing_project_pk)
-    prj = qpm.projects.get(pk=project_pk).qgis_project
+    qpp = QProcessingProject.objects.get(pk=qprocessing_project_pk)
+    prj = qpp.projects.get(pk=project_pk).qgis_project
+
+    qpm = QProcessingModel(str(qpp.model.file))
 
     ctx = QgsProcessingContext()
     ctf = QgsProcessingFeedback()
 
     ctx.setProject(prj)
 
-    print(prj)
-    print(qpm)
-    print(params)
-
     res = qpm.process_algorithm(params, ctx, ctf)
 
     return res
+
+def close_db(fn):
+    """Decorator called by db_task() to be used with tasks that may operate
+    on the database.
+
+    This implementation is a copy of djhuey implementation but it falls
+    back to noop when HUEY.testing is True.
+
+    Set HUEY.testing to True to skip DB connection close.
+
+    """
+
+    @wraps(fn)
+    def inner(*args, **kwargs):
+        try:
+            return fn(*args, **kwargs)
+        finally:
+            if not HUEY.immediate and not getattr(HUEY, 'testing', False):
+                close_old_connections()
+    return inner
+
+
+def db_task(*args, **kwargs):
+    """Decorator to be used with tasks that may operate on the database.
+
+    This implementation is a copy of djhuey implementation but it falls
+    back to noop when HUEY.testing is True.
+
+    Set HUEY.testing to True to skip DB connection close.
+
+    """
+
+    def decorator(fn):
+        ret = task(*args, **kwargs)(close_db(fn))
+        ret.call_local = fn
+        return ret
+    return decorator
 
 @db_task(context=True)
 def run_model_task(qprocessing_project_pk, project_pk, params, task):
@@ -50,5 +104,10 @@ def run_model_task(qprocessing_project_pk, project_pk, params, task):
         task,
         desc='Run Processing Model'
     )
+
+    return run_model(qprocessing_project_pk, project_pk, params)
+
+@shared_task(name='run_model', bind=True)
+def run_model_celery_task(self, qprocessing_project_pk, project_pk, params):
 
     return run_model(qprocessing_project_pk, project_pk, params)
