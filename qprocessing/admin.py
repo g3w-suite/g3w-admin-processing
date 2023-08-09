561fdc5cafc3ae67c753927f03d33a9bbb93bdf4
wlorenzetti <lorenzett@gis3w.it>
2023-08-09 11:21:58 +0200

    Add QProcessingInputUpload model.

--- qprocessing/admin.py
+++ qprocessing/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from guardian.admin import GuardedModelAdmin
-from .models import QProcessingProject
+from .models import QProcessingProject, QProcessingInputUpload
 
 @admin.register(QProcessingProject)
 class QProcessingProjectAdmin(GuardedModelAdmin):
     list_display = (
         'pk',
         'model',
+    )
+
+@admin.register(QProcessingInputUpload)
+class QProcessingInputUploadAdmin(GuardedModelAdmin):
+    list_display = (
+        'uuid',
+        'name',
+        'user'
     )
