7c53fb28197b78660af3edaf522648055f632871
volterra79 <boccacci.francesco@gmail.com>
2025-11-24 17:40:14 +0100

    Remove UploadVectorFile component

--- qprocessing/static/qprocessing/js/components/InputPrjVectorLayer.js
+++ qprocessing/static/qprocessing/js/components/InputPrjVectorLayer.js
@@ -1,4 +1,3 @@
-import UploadVectorFile        from "./UploadVectorFile.js";
 import DrawInputVectorFeatures from "./DrawInputVectorFeatures.js";
 
 const { GUI }                    = g3wsdk.gui;
@@ -21,12 +20,31 @@ export default ({
         <span v-if = "state.validate && state.validate.required">*</span>
       </label>
     </slot>
-    
+
     <section v-if = "showUploadFile" class = "vector-tools-context" v-disabled = "upload">
       <section class = "vector-tools">
-        <upload-vector-file         
-          :upload    = "upload" 
-          @add-layer = "addLayer" />
+        <div
+          class = "qprocessing-upload-vector-file"
+          style = "flex-grow: 2"
+        >
+          <section class = "upload-file-content">
+            <form
+              class                  = "addlayer skin-border-color"
+              v-t-tooltip:top.create = "'mapcontrols.add_layer_control.drag_layer'"
+            >
+              <input
+                ref     = "file"
+                type    = "file"
+                title   = " "
+                @change = "addLayer({ file: $refs.file.files[0], type: 'upload' })"
+                accept  = ".zip,.geojson,.GEOJSON,.kml,.kmz,.KMZ,.KML,.json,.gpx,.gml,.csv"
+              />
+              <div class = "drag_and_drop">
+                <i :class = "g3wtemplate.getFontClass('cloud-upload')" class = "fa-2x" aria-hidden = "true"></i>
+              </div>
+            </form>
+          </section>
+        </div>
         
         <draw-input-vector-features 
           :upload       = "upload" 
@@ -98,7 +116,6 @@ export default ({
   name: "InputPrjVectorLayer",
   mixins: [ selectMixin ],
   components: {
-    UploadVectorFile,
     DrawInputVectorFeatures,
   },
   props: {
@@ -404,5 +421,8 @@ document.head.insertAdjacentHTML(
     .vector-tools                       { display: flex; justify-content: space-between; }
     .vector-tools-message               { margin: 3px; }
     .vector-tools-message .error-upload { font-weight: bold; color: red; }
+    .qprocessing-upload-vector-file form.addlayer   { position: relative; border: 2px dashed; text-align: center; border-radius: 3px; }
+    .qprocessing-upload-vector-file .addlayer input { position: absolute; margin: 0; padding: 0; width: 100%; height: 100%; outline: 0; opacity: 0; cursor: pointer; display: block; }
+    .qprocessing-upload-vector-file .drag_and_drop  { line-height: 20px; padding: 5px; color: #fff; }
   </style>`,
 );
