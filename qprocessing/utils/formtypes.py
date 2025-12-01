8ddc1e8d4155b4afc984b734662fb3b6b8246438
wlorenzetti <lorenzetti@gis3w.it>
2025-12-01 10:01:51 +0100

    Add label to options in QProcessingFormTypeBoolean

--- qprocessing/utils/formtypes.py
+++ qprocessing/utils/formtypes.py
@@ -341,6 +341,7 @@ class QProcessingFormTypeBoolean(QProcessingFormType):
                 'type': self.field_type,
                 'options': {
                     'default': self.default,
+                    'label': self.description,
                     'values': [
                         {'value': 'True', 'checked': True},
                         {'value': 'False', 'checked': False},
